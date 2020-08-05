3190fd3d39b5ffdaf879b1c01b9b8f3bdddc9322
Chris Howell <crhowell@users.noreply.github.com>
2020-08-05 11:30:05 -0700

    Update base project files

--- newrelic_python_kata/urls.py
+++ newrelic_python_kata/urls.py
@@ -1,20 +1,21 @@
-from django.conf.urls import patterns, include, url
-from django.views.generic import TemplateView
 from django.conf import settings
-
-# Uncomment the next two lines to enable the admin:
+from django.conf.urls.static import static
 from django.contrib import admin
-admin.autodiscover()
+from django.urls import include, path
 
-urlpatterns = patterns('',
+from . import views
+
+
+urlpatterns = [
     #url(r'^$', TemplateView.as_view(template_name='base.html')),
     #url(r'^$', TemplateView.as_view(template_name='home.html')),
-    url(r'^$', 'newrelic_python_kata.views.home', name='home'),
-    url(r'^kata1/$', 'employees.views.list', name='list'),
-    url(r'^kata2/$', 'employees.views.filtering', name='query'),
-    url(r'^kata3/$', 'factorial.views.factorial_h', name='factorial'),
-    url(r'^kata4/$', 'weather.views.weather', name='weather'),
-
+    
+    # includes each of the katas into the root
+    path('', include('employees.urls')),
+    path('', include('factorial.urls')),
+    path('', include('weather.urls')),
+    path('admin/', admin.site.urls),
+    path('', views.home),
     # Examples:
     # url(r'^newrelic_python_kata/', include('newrelic_python_kata.foo.urls')),
 
@@ -22,10 +23,8 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
-)
+    # url(r'^admin/', include(admin.site.urls)),
+]
 
-if settings.DEBUG is False:   #if DEBUG is True it will be served automatically
-    urlpatterns += patterns('',
-            url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-            )
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- newrelic_python_kata/views.py
+++ newrelic_python_kata/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.template import RequestContext
 
+
 def home(request):
     return render(request, 'home.html')
